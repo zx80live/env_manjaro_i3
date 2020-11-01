de15b2dc7ff5f594ba70877c5f37542f11e724c9
Andrew Proshkin <zx80live@gmail.com>
2020-11-01 15:37:25 +0300

    go FP template

---
+++ bin/exp/go/src/fp/fp.go
@@ -0,0 +1,59 @@
+package fp
+
+type Any = interface{}
+type Functor func (Any) Any
+type Monad chan Any 
+type Predicate func (Any) bool
+type Option = Monad
+type Traversable = Monad
+
+func Cons(elements ... Any) Monad {
+  c := make(Monad)
+  go func () {
+    defer close(c)
+    for _, e := range elements {
+      c <- e
+    }
+  }()
+
+  return c
+}
+
+func (m Monad) Foreach(fn func(Any)) {
+  for e := range m {
+    fn(e)
+  }
+}
+
+func (m Monad) Map(fn Functor) Monad {
+  c := make(Monad)
+  go func() {
+    defer close(c)
+    for e := range m {
+      c <- fn(e)
+    }
+  }()
+  return c
+}
+
+func (m Monad) Filter(p Predicate) Monad {
+  c := make(Monad)
+
+  go func() {
+    defer close(c)
+    for e:= range m {
+      if res := p(e); res {
+        c <- e
+      }
+    }
+  }()
+
+  return c
+}
+
+func (m Monad) FilterNot(p Predicate) Monad {
+  notP := func(e Any) bool {
+    return !p(e)
+  }
+  return m.Filter(notP)
+}
---
+++ bin/exp/go/src/hello/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  . "fp"
+)
+
+
+func main() {
+  result := Cons(1, -2, 3, -4, -5).Map(func(e Any) Any {
+    return e.(int) * 100
+  }).FilterNot(func(e Any) bool {
+    return e.(int) >= 0
+  })
+
+  result.Foreach(func(e Any){
+    fmt.Println(e)
+  })
+}
