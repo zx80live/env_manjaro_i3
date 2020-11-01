1e1b688683bdf6b3b15d48fde0ea3b812d82acd8
Andrew Proshkin <zx80live@gmail.com>
2020-11-01 19:52:46 +0300

    refactoring

--- bin/exp/todo_go/src/main.go
+++ bin/exp/todo_go/src/main.go
@@ -3,36 +3,26 @@ package main
 import (
   "fmt"
   "regexp"
+  "os"
   . "todo_go/src/fp"
   . "todo_go/src/util"
 )
 
 
 func main() {
-//  result := Cons(1, -2, 3, -4, -5).Map(func(e Any) Any {
-//    return e.(int) * 100
-//  })//.FilterNot(func(e Any) bool {
-//    //return e.(int) >= 0
-//  //})
-//
-//  result.Foreach(func(e Any){
-//    fmt.Println(-e.(int))
-//  })
-
- // res := result.Exists(func(e Any) bool {
- //   return e.(int) == 300
- // })
-
-
- pattern := regexp.MustCompile("")
-
- TodoSelector := func(line Any) bool {
-   return pattern.Match([]byte(line.(string)))
- }
-
- FileLines("/home/temp/doc/worklog/current.md").
-   Filter(TodoSelector).
-   Foreach(func(e Any) {
-     fmt.Println("> " + e.(string))
-   })
+  args := os.Args[1:]
+  filename := args[0]
+  regex := args[1]
+ 
+  pattern := regexp.MustCompile(regex)
+ 
+  TodoSelector := func(line Any) bool {
+    return pattern.Match([]byte(line.(string)))
+  }
+ 
+  FileLines(filename).
+    Filter(TodoSelector).
+    Foreach(func(e Any) {
+      fmt.Println("> " + e.(string))
+    })
 }
